8b343b31e532fc4a23459f5d0985c9ca0bba7940
Max Hurst <max.hurst@hotmail.com>
2020-11-10 19:12:11 +0000

    chore: updated ignores

--- test/config.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.BOT_TOKEN = void 0;
-exports.BOT_TOKEN = 'NzYwOTQ4NDI4OTIxNTAzNzU1.X3Tecw.s8gLhh_v5MgXUOIwOKuUa9tSO1U';
